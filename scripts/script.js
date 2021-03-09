63b3bff6da9f3f32a81dfc702ea2342fb5efbde8
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2021-03-10 02:10:52 +0300

    fix: исправить ошибки закрытия попапа, небольшие изменения

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,26 @@
-let editButton = document.querySelector('.profile__edit-button');
-let addButton = document.querySelector('.profile__add-button');
-let closeButton = document.querySelector('.popup__close-button');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('.popup__form');
-let profileName = document.querySelector('.profile__name');
-let profileNameInput = document.querySelector('.popup__input_name_name');
-let profileCaption = document.querySelector('.profile__caption');
-let profileCaptionInput = document.querySelector('.popup__input_name_caption');
-let likeButton = document.querySelectorAll('.card__like');
-// Popup
-function editButtonHandler() {
-    profileNameInput.value = profileName.textContent;
-    profileCaptionInput.value = profileCaption.textContent;
-    document.querySelector('.popup__edit-profile').classList.add('popup_opened');
-}
-
-function addButtonHandler() {
-    profileNameInput.value = profileName.textContent;
-    profileCaptionInput.value = profileCaption.textContent;
-    document.querySelector('.popup__add-card').classList.add('popup_opened');
+function popupOpen(popupElement) {
+    popupElement.classList.add('popup_opened');
 }
 
 function closeButtonHandler() {
-    popup.classList.remove('popup_opened');
+    document.querySelector('.popup_opened').classList.remove('popup_opened');
 }
+
+// Popup Edit
+const editButton = document.querySelector('.profile__edit-button');
+const closeButtonEdit = document.querySelector('.popup__close-button_action_edit');
+const formElement = document.querySelector('.popup__form');
+const profileName = document.querySelector('.profile__name');
+const profileNameInput = document.querySelector('.popup__input_name_name');
+const profileCaption = document.querySelector('.profile__caption');
+const profileCaptionInput = document.querySelector('.popup__input_name_caption');
+const popupEditProfile = document.querySelector('.popup__edit-profile');
+
+function editButtonHandler() {
+    profileNameInput.value = profileName.textContent;
+    profileCaptionInput.value = profileCaption.textContent;
+    popupOpen(popupEditProfile);
+}    
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -32,10 +29,21 @@ function formSubmitHandler(evt) {
     closeButtonHandler();
 }
 
-
-
 // шпионы
 editButton.addEventListener('click', editButtonHandler);
-addButton.addEventListener('click', addButtonHandler);
-closeButton.addEventListener('click', closeButtonHandler);
+closeButtonEdit.addEventListener('click', closeButtonHandler);
 formElement.addEventListener('submit', formSubmitHandler);
+
+// Popup Add
+const addButton = document.querySelector('.profile__add-button');
+const popupAddCard = document.querySelector('.popup__add-card');
+const closeButtonAdd = document.querySelector('.popup__close-button_action_add');
+
+
+function addButtonHandler() {
+    popupOpen(popupAddCard);
+}
+
+// шпионы
+addButton.addEventListener('click', addButtonHandler);
+closeButtonAdd.addEventListener('click', closeButtonHandler);
