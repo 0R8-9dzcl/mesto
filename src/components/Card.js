5697be59033125636c5214326e0c0b60ee69ce68
0R8-9dzcl <alekseev.evgeniy.wd@yandex.ru>
2021-04-27 15:45:56 +0300

    fix: some fixes

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,35 +3,33 @@ export default class Card {
         this._image = link;
         this._caption = name;
         this._cardSelector = cardSelector;
-        this._cardTitle = сardSetting.cardTitle;
-        this._cardLike = сardSetting.like;
-        this._cardTrash = сardSetting.trashButton;
+        this._card = this._getTemplate();
+        this._cardPhoto = this._card.querySelector(сardSetting.cardPhoto);
+        this._cardLike = this._card.querySelector(сardSetting.like);
+        this._cardTitle = this._card.querySelector(сardSetting.cardTitle);
+        this._cardTrash = this._card.querySelector(сardSetting.trashButton);
         this._handleCardClick = handleCardClick;
-        this._cardPhoto = сardSetting.cardPhoto;
     }
     _getTemplate() {
         const cardElement = this._cardSelector.content.querySelector('.card').cloneNode(true);
         return cardElement;
     }
     _handleLikeCard() {
-        this._like.classList.toggle('card__like_active');
+        this._cardLike.classList.toggle('card__like_active');
     }
     _handleDeleteCard() {
         this._card.remove();
     }
     _setEventListeners() {
-        this._photo.addEventListener('click', () => this._handleCardClick(this._caption, this._image));
-        this._like.addEventListener('click', () => this._handleLikeCard());
-        this._card.querySelector(this._cardTrash).addEventListener('click', () => this._handleDeleteCard());
+        this._cardPhoto.addEventListener('click', () => this._handleCardClick(this._caption, this._image));
+        this._cardLike.addEventListener('click', () => this._handleLikeCard());
+        this._cardTrash.addEventListener('click', () => this._handleDeleteCard());
     }
     generateCard() {
-        this._card = this._getTemplate();
-        this._photo = this._card.querySelector(this._cardPhoto)
-        this._like = this._card.querySelector(this._cardLike)
         this._setEventListeners();
-        this._photo.src = this._image;
-        this._photo.alt = this._caption;
-        this._card.querySelector(this._cardTitle).textContent = this._caption;
+        this._cardPhoto.src = this._image;
+        this._cardPhoto.alt = this._caption;
+        this._cardTitle.textContent = this._caption;
         return this._card;
     }
 }
